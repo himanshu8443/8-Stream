7fbe4240f55275b6cf29fa128f47eeabae34716f
himanshu8443 <sangwanhimanshu8443@gmail.com>
2024-01-16 11:24:51 +0530

    Add PWA support and update metadata

--- next.config.js
+++ next.config.js
@@ -5,4 +5,10 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+});
+
+module.exports = withPWA(nextConfig);
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,9 @@ const poppins = Poppins({
 
 export const metadata: Metadata = {
   title: "8 Stream",
+  generator: "Next.js",
   description: "Watch Movies and TV Shows Online For Free",
+  manifest: "/manifest.json",
 };
 
 export default function RootLayout({
